82c99696d886eb531e638395c58942905a57d6c8
Angelurr <anvixoli@gmail.com>
2026-03-21 12:12:54 -0500

    SCRUM-10 Vista de Registro de Usuarios (RegisterPage)

--- parcial/src/App.jsx
+++ parcial/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginPage from './login_menu/LoginPage'
+import RegisterPage from './register_menu/RegisterPage'
 import './App.css'
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<RegisterPage />} />
       </Routes>
     </Router>
   )
