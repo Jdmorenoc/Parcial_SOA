90ae1bd36634c30e2915a45be5182531ee29f0dd
Juan Diego Moreno Contreras <morenojuand08@gmail.com>
2026-05-06 21:45:03 -0500

    Actualizando datos con jira

---
+++ parcial/src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -0,0 +1,25 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import "./ProtectedRoutes.css";
+
+export function ProtectedRoutes({ children }) {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <div className="loading-card">
+          <div className="spinner"></div>
+          <p className="loading-text">Cargando...</p>
+        </div>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  
+  return children;
+}
